b1281c00464956fe3c84d167d51baf0809f020b9
Ignacio Lopezosa Serrano <n4xo@pm.me>
2022-07-09 20:22:56 +0200

    Hello Express

--- myApp.js
+++ myApp.js
@@ -1,7 +1,7 @@
 let express = require('express');
 let app = express();
 
-app.get("/", (req, res) => res.send("Response String"))
+app.get("/", (req, res) => res.send("Hello Express"))
 
 (() => {console.log('Hello World');})()
 
