a849d9c4f99876b178b06c839ef8cd4654356c96
Ignacio Lopezosa Serrano <n4xo@pm.me>
2022-07-10 12:47:53 +0200

    Use the .env File, process.env

--- myApp.js
+++ myApp.js
@@ -3,16 +3,16 @@ require('dotenv').config();
 let express = require('express');
 let app = express();
 
-console.log('log: ' + process.env.MESSAGE_STYLE);
-// Index route
-app.get("/", (req, res) => res.sendFile(__dirname + '/views/index.html'));
+console.log('log: ' + process.env);
 // Static CSS resources
 app.use('/public', express.static(__dirname + '/public'));
 // JSON API
 app.get('/json', (req, res) => res.json(
     process.env.MESSAGE_STYLE === 'uppercase' ? { message: 'HELLO JSON' } : { message: 'Hello json' }
-));
-
+    ));
+// Index route
+app.get("/", (req, res) => res.sendFile(__dirname + '/views/index.html'));
+    
 
 
 
