1da8a97bdf84a26d39f380e281a628e99b5d6f5a
Ignacio Lopezosa Serrano <n4xo@pm.me>
2022-07-10 12:37:04 +0200

    Use the .env File, dotenv log fix

--- myApp.js
+++ myApp.js
@@ -11,7 +11,7 @@ app.get('/json', (req, res) => res.json(
     process.env.MESSAGE_STYLE === 'uppercase' ? { message: 'HELLO JSON' } : { message: 'Hello json' }
 ));
 
-console.log(process.env.MESSAGE_STYLE);
+console.log('log: ' + process.env.MESSAGE_STYLE);
 
 
 
