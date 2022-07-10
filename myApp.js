fb92686ac2bd5777df5a43230353d171b5566418
Ignacio Lopezosa Serrano <n4xo@pm.me>
2022-07-10 13:21:17 +0200

    Implement a Root-Level Request Logger Middleware

--- myApp.js
+++ myApp.js
@@ -4,12 +4,14 @@ let express = require('express');
 let app = express();
 
 console.log(process.env);
+// Request Logger
+app.use('/', (req, res, next) => { console.log(`${req.method} ${req.path} - ${req.ip}`); next(); });
 // Static CSS resources
 app.use('/public', express.static(__dirname + '/public'));
 // JSON API
 app.get('/json', (req, res) => res.json(
     process.env.MESSAGE_STYLE === 'uppercase' ? { message: 'HELLO JSON' } : { message: 'Hello json' }
-    ));
+));
 // Index route
 app.get("/", (req, res) => res.sendFile(__dirname + '/views/index.html'));
     
