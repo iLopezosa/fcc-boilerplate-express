5d13964869c6c304f2f95c55f5c47165cdf337c5
Ignacio Lopezosa Serrano <n4xo@pm.me>
2022-07-10 12:09:10 +0200

    Serve JSON on a Specific Route

--- myApp.js
+++ myApp.js
@@ -1,8 +1,12 @@
 let express = require('express');
 let app = express();
-
+// Index route
 app.get("/", (req, res) => res.sendFile(__dirname + '/views/index.html'));
+// Static CSS resources
 app.use('/public', express.static(__dirname + '/public'));
+// JSON API
+app.get('/json', (req, res) => res.json({ message: 'Hello json' }));
+
 
 
 
