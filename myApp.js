a56260b122964353171724051371d0a721a5b83d
Ignacio Lopezosa Serrano <n4xo@pm.me>
2022-07-10 12:26:44 +0200

    Use the .env File

--- myApp.js
+++ myApp.js
@@ -5,7 +5,9 @@ app.get("/", (req, res) => res.sendFile(__dirname + '/views/index.html'));
 // Static CSS resources
 app.use('/public', express.static(__dirname + '/public'));
 // JSON API
-app.get('/json', (req, res) => res.json({ message: 'Hello json' }));
+app.get('/json', (req, res) => res.json(
+    process.env.MESSAGE_STYLE === 'uppercase' ? { message: 'HELLO JSON' } : { message: 'Hello json' }
+));
 
 
 
